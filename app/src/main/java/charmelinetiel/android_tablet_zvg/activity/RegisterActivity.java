990f5f9a19c9c830625f981bd2a30192d55f42fc
C tiel <c.tiel@hotmail.com>
2017-11-27 20:51:10 +0100

    meh

--- app/src/main/java/charmelinetiel/android_tablet_zvg/activity/RegisterActivity.java
+++ app/src/main/java/charmelinetiel/android_tablet_zvg/activity/RegisterActivity.java
@@ -9,16 +9,26 @@ import android.view.View;
 import com.stepstone.stepper.StepperLayout;
 import com.stepstone.stepper.VerificationError;
 
+import java.util.List;
+
 import charmelinetiel.android_tablet_zvg.R;
 import charmelinetiel.android_tablet_zvg.adapters.RegisterAdapter;
 import charmelinetiel.android_tablet_zvg.fragments.RegisterCompletedFragment;
 import charmelinetiel.android_tablet_zvg.fragments.RegisterStep1Fragment;
 import charmelinetiel.android_tablet_zvg.fragments.RegisterStep2Fragment;
+import charmelinetiel.android_tablet_zvg.models.Consultant;
+import charmelinetiel.android_tablet_zvg.webservices.APIService;
+import charmelinetiel.android_tablet_zvg.webservices.RetrofitClient;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
-public class RegisterActivity extends AppCompatActivity implements StepperLayout.StepperListener {
+public class RegisterActivity extends AppCompatActivity implements Callback<List<Consultant>>, StepperLayout.StepperListener {
 
     private StepperLayout mStepperLayout;
     private RegisterAdapter mStepperAdapter;
+    private APIService apiService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +40,18 @@ public class RegisterActivity extends AppCompatActivity implements StepperLayout
         mStepperLayout.setAdapter(mStepperAdapter);
         mStepperLayout.setListener(this);
 
+
+        Retrofit retrofit = RetrofitClient.getClient("https://zvh-api.herokuapp.com/");
+
+        apiService = retrofit.create(APIService.class);
+
+        fetchContent();
+
+    }
+
+    private void fetchContent() {
+
+        apiService.getAllConsultants().enqueue(this);
     }
 
     @Override
@@ -88,4 +110,15 @@ public class RegisterActivity extends AppCompatActivity implements StepperLayout
         fgTransition.commit();
     }
 
+    @Override
+    public void onResponse(Call<List<Consultant>> call, Response<List<Consultant>> response) {
+        if (response.isSuccessful() && response.body() != null) {
+            List<Consultant> allConsultants = response.body();
+        }
+    }
+
+    @Override
+    public void onFailure(Call<List<Consultant>> call, Throwable t) {
+        
+    }
 }
