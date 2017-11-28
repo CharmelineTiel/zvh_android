da43ec36fb1f0e0c6147cb907db092d73ce75b4e
C tiel <c.tiel@hotmail.com>
2017-11-28 18:34:26 +0100

    dagboek layout

--- app/src/main/java/charmelinetiel/android_tablet_zvg/adapters/ListAdapter.java
+++ app/src/main/java/charmelinetiel/android_tablet_zvg/adapters/ListAdapter.java
@@ -1,8 +1,105 @@
 package charmelinetiel.android_tablet_zvg.adapters;
 
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import charmelinetiel.android_tablet_zvg.R;
+import charmelinetiel.android_tablet_zvg.models.Measurement;
+
 /**
  * Created by Tiel on 27-11-2017.
  */
 
-public class ListAdapter {
+public class ListAdapter extends BaseAdapter {
+
+    private static LayoutInflater inflater = null;
+    private Fragment fg;
+    private Context context;
+    private ArrayList<Measurement> data;
+
+    private static class ViewHolder {
+        TextView title;
+        TextView bloodPressure;
+        TextView date;
+        TextView feedbackMessage;
+        RelativeLayout layout;
+    }
+
+    public ListAdapter(Context context, Fragment fg, ArrayList<Measurement> data) {
+        this.fg = fg;
+        this.data = data;
+        this.inflater = fg.getLayoutInflater();
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        Measurement m = data.get(position);
+        ViewHolder viewHolder;
+
+        if (convertView == null) {
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(context);
+            convertView = inflater.inflate(R.layout.measurement_list, null);
+
+            viewHolder.title = convertView.findViewById(R.id.titleLbl);
+            viewHolder.bloodPressure = convertView.findViewById(R.id.bloodPressureLbl);
+            viewHolder.date = convertView.findViewById(R.id.dateLbl);
+            viewHolder.feedbackMessage = convertView.findViewById(R.id.feedbackLbl);
+            viewHolder.layout = convertView.findViewById(R.id.measurement_list_item);
+
+            convertView.setTag(viewHolder);
+        }else{
+
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+
+        viewHolder.title.setText(m.getMeasurementDateTime());
+        viewHolder.bloodPressure.setText("Bovendruk:" + m.getBloodPressureUpper().toString() + "," + " " +
+                "Onderdruk:" +
+                m.getBloodPressureLower().toString());
+        //viewHolder.date.setText(m.getMeasurementDateTime());
+
+        if (m.getBloodPressureLower() <= 100 && m.getBloodPressureUpper()
+                <= 130) {
+
+            viewHolder.feedbackMessage.setTextColor(ContextCompat.getColor(context, R.color.positiveFeedbackTxt));
+            viewHolder.feedbackMessage.setText("Uw bloeddruk was prima");
+            viewHolder.layout.setBackgroundResource(R.color.positiveFeedback);
+        }else{
+
+            viewHolder.feedbackMessage.setTextColor(ContextCompat.getColor(context, R.color.negativeFeedbackTxt));
+            viewHolder.feedbackMessage.setText("Houd uw bloeddruk goed in de gaten");
+            viewHolder.layout.setBackgroundResource(R.color.negativeFeedback);
+        }
+            return convertView;
+
+
+    }
 }
--- app/src/main/java/charmelinetiel/android_tablet_zvg/fragments/DiaryFragment.java
+++ app/src/main/java/charmelinetiel/android_tablet_zvg/fragments/DiaryFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.ListView;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.data.BarData;
@@ -16,6 +18,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import charmelinetiel.android_tablet_zvg.R;
+import charmelinetiel.android_tablet_zvg.adapters.ListAdapter;
+import charmelinetiel.android_tablet_zvg.models.Measurement;
 
 
 /**
@@ -24,6 +28,10 @@ import charmelinetiel.android_tablet_zvg.R;
 public class DiaryFragment extends Fragment {
 
     private BarChart chart;
+    private ListView mListView;
+    private ListAdapter adapter;
+    private View v;
+    private ArrayList<Measurement> listItems;
 
     public DiaryFragment() {
         // Required empty public constructor
@@ -34,43 +42,96 @@ public class DiaryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View v = inflater.inflate(R.layout.fragment_diary, container, false);
+        v = inflater.inflate(R.layout.fragment_diary, container, false);
+        mListView = v.findViewById(R.id.measurement_list_view);
+        dummyData();
 
+        mListView.setOnScrollListener(new AbsListView.OnScrollListener() {
+
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem,
+                                 int visibleItemCount, int totalItemCount) {
+
+            }
+        });
         chart = v.findViewById(R.id.chart);
-
         initGraph();
         return v;
     }
 
+    public void onActivityCreated(Bundle savedInstanceState)
+    {
+        super.onActivityCreated(savedInstanceState);
+    }
 
+    public void dummyData()
+    {
+        listItems = new ArrayList<>();
+
+        Measurement m1 = new Measurement();
+        m1.setBloodPressureUpper(120);
+        m1.setBloodPressureLower(80);
+        m1.setMeasurementDateTime("di, 28 nov 2017");
+        listItems.add(m1);
+
+        Measurement m2 = new Measurement();
+        m2.setBloodPressureUpper(150);
+        m2.setBloodPressureLower(80);
+        m2.setMeasurementDateTime("ma, 27 nov 2017");
+        listItems.add(m2);
+
+        Measurement m3 = new Measurement();
+        m3.setBloodPressureUpper(160);
+        m3.setBloodPressureLower(60);
+        m3.setMeasurementDateTime("zo, 26 nov 2017");
+        listItems.add(m3);
+
+        Measurement m4 = new Measurement();
+        m4.setBloodPressureUpper(100);
+        m4.setBloodPressureLower(80);
+        m4.setMeasurementDateTime("za, 25 nov 2017");
+        listItems.add(m4);
+
+
+        adapter = new ListAdapter(getActivity(),this, listItems);
+        mListView.setAdapter(adapter);
+
+    }
     public void initGraph(){
 
-        //list 1
-        List<BarEntry> entries = new ArrayList<>();
-        entries.add(new BarEntry(0f, 30f));
-        entries.add(new BarEntry(1f, 80f));
-        entries.add(new BarEntry(2f, 60f));
-        entries.add(new BarEntry(3f, 50f));
-        // gap of 2f
-        entries.add(new BarEntry(5f, 70f));
-        entries.add(new BarEntry(6f, 60f));
+        //bovendruk
+        List<BarEntry> bloodPressureUpper = new ArrayList<>();
+        for (int i = 0; i < listItems.size(); i++)
+        {
+            bloodPressureUpper.add(new BarEntry(i,listItems.get(i).getBloodPressureUpper()));
+        }
 
-        //list 2
-        List<BarEntry> entries2 = new ArrayList<>();
-        entries2.add(new BarEntry(10f, 34f));
-        entries2.add(new BarEntry(20f, 30f));
-        entries2.add(new BarEntry(5f, 70f));
-        entries2.add(new BarEntry(8f, 55f));
-        // gap of 2f
-        entries2.add(new BarEntry(9f, 74f));
-        entries2.add(new BarEntry(8f, 66f));
+        //onderdruk
+        List<BarEntry> bloodPressureLower = new ArrayList<>();
+        for (int i = 0; i < listItems.size(); i++)
+        {
+            bloodPressureLower.add(new BarEntry(i,listItems.get(i).getBloodPressureLower()));
+        }
 
-        BarDataSet set1 = new BarDataSet(entries, "Bovendruk");
-        BarDataSet set2 = new BarDataSet(entries2, "Onderdruk");
+        BarDataSet upperBP = new BarDataSet(bloodPressureUpper, "Bovendruk");
+        BarDataSet lowerBP = new BarDataSet(bloodPressureLower, "Onderdruk");
 
-        BarData data = new BarData(set1,set2);
+        int color1 = getResources().getColor(R.color.chart2);
+        int color2 = getResources().getColor(R.color.chart1);
+
+        lowerBP.setColors(color1);
+        upperBP.setColors(color2);
+
+        BarData data = new BarData(upperBP,lowerBP);
         data.setBarWidth(0.3f); // set custom bar width
+        chart.setDrawGridBackground(false);
         chart.setData(data);
+        chart.animateXY(2000, 2000);
         chart.setFitBars(true); // make the x-axis fit exactly all bars
         chart.invalidate(); // refresh
     }
