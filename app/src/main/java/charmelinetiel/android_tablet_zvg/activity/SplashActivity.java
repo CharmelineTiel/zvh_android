ba7e050830ea5a6d64909a4dae8190c91e77af00
DESKTOP-AMP2N8L\youp <youpkuiper@gmail.com>
2017-12-18 10:39:17 +0100

    settings begin

--- app/src/main/java/charmelinetiel/android_tablet_zvg/activity/SplashActivity.java
+++ app/src/main/java/charmelinetiel/android_tablet_zvg/activity/SplashActivity.java
@@ -1,7 +1,9 @@
 package charmelinetiel.android_tablet_zvg.activity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
@@ -9,6 +11,12 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Check if Auto-login has been configured
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        //Check if user has been saved, if so go to mainactivity
+
+
         Intent intent = new Intent(this, charmelinetiel.android_tablet_zvg.activity.RegisterActivity.class);
         startActivity(intent);
         finish();
--- app/src/main/java/charmelinetiel/android_tablet_zvg/fragments/ServiceFragment.java
+++ app/src/main/java/charmelinetiel/android_tablet_zvg/fragments/ServiceFragment.java
@@ -1,8 +1,10 @@
 package charmelinetiel.android_tablet_zvg.fragments;
 
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 
 import charmelinetiel.android_tablet_zvg.R;
@@ -11,7 +13,7 @@ import charmelinetiel.android_tablet_zvg.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ServiceFragment extends PreferenceFragmentCompat {
+public class ServiceFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
 
 
     public ServiceFragment() {
@@ -22,6 +24,16 @@ public class ServiceFragment extends PreferenceFragmentCompat {
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
 
         addPreferencesFromResource(R.xml.app_preferences);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+                                          String key) {
+        if (key.equals("kjh")) {
+            Preference connectionPref = findPreference(key);
+            // Set summary to be the user-description for the selected value
+            connectionPref.setSummary(sharedPreferences.getString(key, ""));
+        }
     }
 
 }
