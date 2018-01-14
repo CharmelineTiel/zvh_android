d7f56e897847840a8a7f117f1e14c1b52b75ffbe
DESKTOP-AMP2N8L\youp <youpkuiper@gmail.com>
2018-01-14 22:34:00 +0100

    auto login met smart lock opzet

--- app/src/main/java/charmelinetiel/android_tablet_zvg/activity/LoginActivity.java
+++ app/src/main/java/charmelinetiel/android_tablet_zvg/activity/LoginActivity.java
@@ -3,16 +3,18 @@ package charmelinetiel.android_tablet_zvg.activity;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.content.IntentSender;
 import android.net.Uri;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -21,12 +23,24 @@ import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.credentials.Credential;
+import com.google.android.gms.auth.api.credentials.CredentialRequest;
+import com.google.android.gms.auth.api.credentials.CredentialRequestResult;
+import com.google.android.gms.auth.api.credentials.IdentityProviders;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 
 import org.json.JSONObject;
 
 import charmelinetiel.android_tablet_zvg.R;
-import charmelinetiel.android_tablet_zvg.models.ExceptionHandler;
 import charmelinetiel.android_tablet_zvg.models.AuthToken;
+import charmelinetiel.android_tablet_zvg.models.ExceptionHandler;
 import charmelinetiel.android_tablet_zvg.models.FormErrorHandling;
 import charmelinetiel.android_tablet_zvg.models.User;
 import charmelinetiel.android_tablet_zvg.webservices.APIService;
@@ -37,7 +51,15 @@ import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener, Callback<User> {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener, Callback<User>, GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+    private static final String TAG = "LoginActivity";
+    private static final int RC_SAVE = 1;
+    private static final int RC_READ = 3;
+    private static final String IS_RESOLVING = "is_resolving";
+    private static final String IS_REQUESTING = "is_requesting";
+    private static final String ACCOUNT_TYPE = "zorgvoorhethart.account";
 
     private APIService apiService;
     private TextView forgotPassword;
@@ -47,12 +69,28 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private Uri data;
     private ProgressBar progressBar;
     private ScrollView loginPage;
+    private GoogleApiClient mGoogleApiClient;
+    private User loggedInUser;
+    private boolean mIsResolving;
+    private boolean mIsRequesting;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_login);
+
+        if (savedInstanceState != null) {
+            mIsResolving = savedInstanceState.getBoolean(IS_RESOLVING);
+            mIsRequesting = savedInstanceState.getBoolean(IS_REQUESTING);
+        }
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .enableAutoManage(this, 0, this)
+                .addApi(Auth.CREDENTIALS_API)
+                .build();
 
         Retrofit retrofit = RetrofitClient.getClient();
         apiService = retrofit.create(APIService.class);
@@ -116,7 +154,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                         user.setPassword(password.getText().toString());
                         user.setEmailAddress(email.getText().toString());
 
-                    if(user != null && validInput()) {
+                    if(validInput()) {
                         showProgressBar();
                         apiService.login(user).enqueue(this);
                     }
@@ -207,7 +245,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
                 break;
         }
-
     }
 
     @Override
@@ -240,23 +277,22 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     public void onResponse(Call<User> call, Response<User> response) {
         if (response.body() != null && response.isSuccessful()) {
 
-            User loggedInUser = response.body();
+            loggedInUser = response.body();
 
             AuthToken.getInstance().setAuthToken(response.body().getAuthToken());
 
             loggedInUser.setAuthToken(response.body().getAuthToken());
-            Intent intent = new Intent(this, MainActivity.class);
-            intent.putExtra("user", loggedInUser);
-            startActivity(intent);
 
             if (autoLoginCheckBox.isChecked()) {
 
-                SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
-                SharedPreferences.Editor editor = sharedPref.edit();
-                editor.putBoolean("autoLogin", true);
-                editor.putString("emailAddress", email.getText().toString());
-                editor.putString("password", password.getText().toString());
-                editor.apply();
+                Credential credential = new Credential.Builder(email.getText().toString())
+                        .setPassword(password.getText().toString())
+                        .build();
+                        .build();
+                saveCredential(credential);
+            }else{
+                goToMainActivity();
+
             }
         } else {
             hideProgressBar();
@@ -279,7 +315,41 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
             makeSnackBar(ExceptionHandler.getMessage(e), this);
         }
+    }
 
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        // Save the user's current sign in state
+        savedInstanceState.putBoolean(IS_RESOLVING, mIsResolving);
+        savedInstanceState.putBoolean(IS_REQUESTING, mIsRequesting);
+
+        // Always call the superclass to save the view hierarchy state
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "onActivityResult:" + requestCode + ":" + resultCode + ":" + data);
+
+        if (requestCode == RC_READ) {
+            if (resultCode == RESULT_OK) {
+                Credential credential = data.getParcelableExtra(Credential.EXTRA_KEY);
+                processRetrievedCredential(credential);
+            } else {
+                Log.e(TAG, "Credential Read: NOT OK");
+                hideProgressBar();
+            }
+        } else if (requestCode == RC_SAVE) {
+            Log.d(TAG, "Result code: " + resultCode);
+            if (resultCode == RESULT_OK) {
+                Log.d(TAG, "Credential Save: OK");
+            } else {
+                Log.e(TAG, "Credential Save Failed");
+            }
+            goToMainActivity();
+        }
+        mIsResolving = false;
     }
 
     public void openFragment(final Fragment fg)
@@ -309,4 +379,169 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         snackbar.show();
     }
 
+    protected void saveCredential(Credential credential) {
+        // Credential is valid so save it.
+        Auth.CredentialsApi.save(mGoogleApiClient,
+                credential).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(Status status) {
+                if (status.isSuccess()) {
+                    Log.d(TAG, "Credential saved");
+                    goToMainActivity();
+                } else {
+                    Log.d(TAG, "Attempt to save credential failed " +
+                            status.getStatusMessage() + " " +
+                            status.getStatusCode());
+                    resolveResult(status, RC_SAVE);
+                }
+            }
+        });
+    }
+
+    private void deleteCredential(Credential credential) {
+        Auth.CredentialsApi.delete(mGoogleApiClient,
+                credential).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(Status status) {
+                if (status.isSuccess()) {
+                    Log.d(TAG, "Credential successfully deleted.");
+                } else {
+                    // This may be due to the credential not existing, possibly
+                    // already deleted via another device/app.
+                    Log.d(TAG, "Credential not deleted successfully.");
+                }
+            }
+        });
+    }
+
+    private void resolveResult(Status status, int requestCode) {
+        // We don't want to fire multiple resolutions at once since that can result
+        // in stacked dialogs after rotation or another similar event.
+        if (mIsResolving) {
+            Log.w(TAG, "resolveResult: already resolving.");
+            return;
+        }
+
+        Log.d(TAG, "Resolving: " + status);
+        if (status.hasResolution()) {
+            Log.d(TAG, "STATUS: RESOLVING");
+            try {
+                status.startResolutionForResult(this, requestCode);
+                mIsResolving = true;
+            } catch (IntentSender.SendIntentException e) {
+                Log.e(TAG, "STATUS: Failed to send resolution.", e);
+            }
+        } else {
+            goToMainActivity();
+        }
+    }
+
+    private void processRetrievedCredential(Credential credential) {
+        String accountType = credential.getAccountType();
+        if(accountType == null) {
+            User user = new User();
+            user.setEmailAddress(credential.getId());
+            user.setPassword(credential.getPassword());
+            apiService.login(user).enqueue(this);
+
+            //TODO: iets doen als de opgehaalde credentials niet goed zijn
+            if(!true) {
+                // This is likely due to the credential being changed outside of
+                // Smart Lock,
+                // ie: away from Android or Chrome. The credential should be deleted
+                // and the user allowed to enter a valid credential.
+                Log.d(TAG, "Retrieved credential invalid, so delete retrieved" +
+                        " credential.");
+                Toast.makeText(this, "Retrieved credentials are invalid, so will be deleted.", Toast.LENGTH_LONG).show();
+                deleteCredential(credential);
+                requestCredentials();
+//                mSignInButton.setEnabled(false);
+            }
+        }
+        else if (accountType.equals(IdentityProviders.GOOGLE)) {
+            // The user has previously signed in with Google Sign-In. Silently
+            // sign in the user with the same ID.
+            // See https://developers.google.com/identity/sign-in/android/
+//                    GoogleSignInOptions gso =
+//                            new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//                                    .requestEmail()
+//                                    .build();
+//                    mGoogleApiClient = new GoogleApiClient.Builder(this)
+//                            .enableAutoManage(this, this)
+//                            .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+//                            .setAccountName(credential.getId())
+//                            .build();
+//                    OptionalPendingResult<GoogleSignInResult> opr =
+//                            Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
+            // ...
+        }
+    }
+
+    private void requestCredentials() {
+        mIsRequesting = true;
+
+        CredentialRequest request = new CredentialRequest.Builder()
+                .setPasswordLoginSupported(true)
+                .setAccountTypes(IdentityProviders.GOOGLE, IdentityProviders.TWITTER)
+                .build();
+
+        Auth.CredentialsApi.request(mGoogleApiClient, request).setResultCallback(
+                new ResultCallback<CredentialRequestResult>() {
+                    @Override
+                    public void onResult(CredentialRequestResult credentialRequestResult) {
+//                        mIsRequesting = false;
+                        Status status = credentialRequestResult.getStatus();
+                        if (credentialRequestResult.getStatus().isSuccess()) {
+                            // Successfully read the credential without any user interaction, this
+                            // means there was only a single credential and the user has auto
+                            // sign-in enabled.
+                            Credential credential = credentialRequestResult.getCredential();
+                            processRetrievedCredential(credential);
+                        } else if (status.getStatusCode() == CommonStatusCodes.RESOLUTION_REQUIRED) {
+//                            setFragment(null);
+                            // This is most likely the case where the user has multiple saved
+                            // credentials and needs to pick one.
+                            resolveResult(status, RC_READ);
+                        } else if (status.getStatusCode() == CommonStatusCodes.SIGN_IN_REQUIRED) {
+//                            setFragment(null);
+                            // This is most likely the case where the user does not currently
+                            // have any saved credentials and thus needs to provide a username
+                            // and password to sign in.
+                            Log.d(TAG, "Sign in required");
+                        } else {
+                            Log.w(TAG, "Unrecognized status code: " + status.getStatusCode());
+//                            setFragment(null);
+                        }
+                    }
+                }
+        );
+    }
+
+    public void goToMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("user", loggedInUser);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.d(TAG, "onConnected");
+
+        // Request Credentials once connected. If credentials are retrieved
+        // the user will either be automatically signed in or will be
+        // presented with credential options to be used by the application
+        // for sign in.
+        requestCredentials();
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        Log.d(TAG, "onConnectionSuspended: " + cause);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed: " + connectionResult);
+    }
 }
